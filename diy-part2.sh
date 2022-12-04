b0d09bc21e0756813790bf72ce7086ed73f5a5de
oceanromain <starinternet@163.com>
2022-12-04 11:37:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,18 +18,15 @@ sed -i "/uci commit system/i\uci set system.@system[0].hostname='Openwrt-BY'" pa
 sed -i "s/hostname='OpenWrt'/hostname='Openwrt-BY'/g" ./package/base-files/files/bin/config_generate
 echo "net.netfilter.nf_conntrack_max=165535" >> package/base-files/files/etc/sysctl.conf 
 rm -rf package/lean/autocore
-echo "     _________                                                " > package/base-files/files/etc/banner
-echo "    /        /\      _    ___ ___  ___                        " >> package/base-files/files/etc/banner
-echo "   /  LE    /  \    | |  | __|   \| __|                       " >> package/base-files/files/etc/banner
-echo "  /    DE  /    \   | |__| _|| |) | _|                        " >> package/base-files/files/etc/banner
-echo " /________/  LE  \  |____|___|___/|___|                       " >> package/base-files/files/etc/banner
-echo " \        \   DE /                                            " >> package/base-files/files/etc/banner
-echo "  \    LE  \    /  -------------------------------------------" >> package/base-files/files/etc/banner
-echo "   \  DE    \  /    %D %V, %C                                 " >> package/base-files/files/etc/banner
-echo "    \________\/    -------------------------------------------" >> package/base-files/files/etc/banner
-echo "--------------------------------------------------------------" >> package/base-files/files/etc/banner
-echo "                   Openwrt by Starnet                         " >> package/base-files/files/etc/banner
-echo "--------------------------------------------------------------" >> package/base-files/files/etc/banner
+echo " ____  _                        _      ___                __        __    _   " > package/base-files/files/etc/banner
+echo "/ ___|| |_ __ _ _ __ _ __   ___| |_   / _ \ _ __   ___ _ _\ \      / / __| |_ " >> package/base-files/files/etc/banner
+echo "\___ \| __/ _\` | '__| '_ \ / _ \ __| | | | | '_ \ / _ \ '_ \ \ /\ / / '__| __|" >> package/base-files/files/etc/banner
+echo " ___) | || (_| | |  | | | |  __/ |_  | |_| | |_) |  __/ | | \ V  V /| |  | |_ " >> package/base-files/files/etc/banner
+echo "|____/ \__\__,_|_|  |_| |_|\___|\__|  \___/| .__/ \___|_| |_|\_/\_/ |_|   \__|" >> package/base-files/files/etc/banner
+echo "                                           |_|                                " >> package/base-files/files/etc/banner
+echo "------------------------------------------------------------------------------" >> package/base-files/files/etc/banner
+echo "                        %D %V %C                         " >> package/base-files/files/etc/banner
+echo "------------------------------------------------------------------------------" >> package/base-files/files/etc/banner
 
 #增加设置向导
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-wizard package/luci-app-wizard
@@ -37,6 +34,10 @@ svn co https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotim
 svn co https://github.com/oceanromain/openwrt-packages-1/trunk/luci-app-beardropper package/luci-app-beardropper
 svn co https://github.com/oceanromain/openwrt-packages-1/trunk/luci-app-cowbping package/luci-app-cowbping
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-rebootschedule package/luci-app-rebootschedule
+
+#修改
+sed -i '7s/control/services/g' package/luci-app-rebootschedule/luasrc/controller/rebootschedule.lua 
+sed -i 's/R22.11.11/by Starnet 202212/g' ./package/lean/default-settings/files/zzz-default-settings
 
 #安装
 ./scripts/feeds install -af
