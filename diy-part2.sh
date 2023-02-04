c8199e6974cd6857fed89dea06afffbb8be0035a
oceanromain <starinternet@163.com>
2023-02-04 17:31:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -35,7 +35,7 @@ svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-wizard packag
 svn co https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 svn co https://github.com/oceanromain/openwrt-packages-1/trunk/luci-app-beardropper package/luci-app-beardropper
 svn co https://github.com/oceanromain/openwrt-packages-1/trunk/luci-app-cowbping package/luci-app-cowbping
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-rebootschedule package/luci-app-rebootschedule
+svn co https://github.com/m16-spsad/sirpdboy-package/trunk/luci-app-rebootschedule package/luci-app-rebootschedule
 
 #修改
 #sed -i '7s/control/system/g' package/luci-app-rebootschedule/luasrc/controller/rebootschedule.lua 
