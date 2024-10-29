deb1d88630ce417cf2ea10128440de40cf577142
18601121069 <startresnet@163.com>
2024-10-30 06:54:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,8 +20,8 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git nas https://github.com/linkease/nas-packages.git' >>feeds.conf.default
 
-#echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
 #echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >>feeds.conf.default
 #echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >>feeds.conf.default
 #echo 'src-git ddns_go https://github.com/sirpdboy/luci-app-ddns-go' >>feeds.conf.default
@@ -32,3 +32,4 @@ echo 'src-git oceanweb https://github.com/oceanromain/my-openwrt-package.git' >>
 #20241025
 #echo 'src-git small_all https://github.com/kenzok8/small-package.git' >> feeds.conf.default
 #echo 'src-git nxx https://github.com/NueXini/NueXini_Packages.git' >> feeds.conf.default
+sed -i '/;openwrt-23.05/d' feeds.conf.default
