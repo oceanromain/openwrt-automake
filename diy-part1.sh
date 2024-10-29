08d8862ad40ebfbfa8917a4de05c6a25009356b9
18601121069 <startresnet@163.com>
2024-10-30 06:16:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,14 +20,14 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git nas https://github.com/linkease/nas-packages.git' >>feeds.conf.default
 
-echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
 #echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >>feeds.conf.default
 #echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >>feeds.conf.default
 #echo 'src-git ddns_go https://github.com/sirpdboy/luci-app-ddns-go' >>feeds.conf.default
 #echo 'src-git autocore https://github.com/oceanromain/myautocore package/myautocore' >>feeds.conf.default
 echo 'src-git oceanweb https://github.com/oceanromain/my-openwrt-package.git' >>feeds.conf.default
-echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> feeds.conf.default
+#echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> feeds.conf.default
 #echo 'src-git small_all https://github.com/kenzok8/small-package.git' >> feeds.conf.default
 #20241025
 #echo 'src-git small_all https://github.com/kenzok8/small-package.git' >> feeds.conf.default
